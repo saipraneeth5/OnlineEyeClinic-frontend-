81f61fc20874745428ef509fd8c7ed7e9c6158d2
Praneeth Kondraju <30432648+saipraneeth5@users.noreply.github.com>
2022-03-19 05:45:29 +0530

    Revert "OnlineEyeClinic_Frontend_Final"

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# OnlineEyeClinicUI
+# OnlineEyeClinic
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.4.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/OnlineEyeClinicUI'),
+      dir: require('path').join(__dirname, './coverage/OnlineEyeClinic'),
       subdir: '.',
       reporters: [
         { type: 'html' },
